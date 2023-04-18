5e60ba3274785e01800e0a693f3f9498df4dcb6f
ElliotRipa <elliot.ripa@gmail.com>
2023-04-18 11:38:09 +0200

    Add funcitonality to connect to MySQL Database.

--- database/src/Main.java
+++ database/src/Main.java
@@ -1,9 +1,36 @@
 package src;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+
+        connectMySQL("SELECT * FROM country");
+
     }
+
+    static private void connectMySQL(String statement) {
+
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/world","root", "password");
+            Statement stmt = con.createStatement();
+            ResultSet result = stmt.executeQuery(statement);
+
+            System.out.println("Hello");
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
 
 }
